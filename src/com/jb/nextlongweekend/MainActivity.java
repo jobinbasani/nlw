3bd402ec5d469e82d9b1f928595c926609e2e816
jobinbasani <jobinbasani@gmail.com>
2013-11-16 22:38:53 -0400

    Add Spinner listener

--- src/com/jb/nextlongweekend/MainActivity.java
+++ src/com/jb/nextlongweekend/MainActivity.java
@@ -2,21 +2,66 @@ package com.jb.nextlongweekend;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 public class MainActivity extends Activity {
+	
+	SharedPreferences prefs;
+	final public static String COUNTRY_KEY = "country";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		prefs = getPreferences(MODE_PRIVATE);
+		init();
+		loadPreferences();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	private void loadPreferences(){
+		String defaultCountry = prefs.getString(COUNTRY_KEY, "USA");
+		Spinner countrySpinner = (Spinner) findViewById(R.id.countrySelector);
+		SpinnerAdapter countryArray = countrySpinner.getAdapter();
+		int position = -1;
+		for(int i=0;i<countryArray.getCount();i++){
+			if(countryArray.getItem(i).toString().equalsIgnoreCase(defaultCountry)){
+				position = i;
+				break;
+			}
+		}
+		if(position>=0){
+			countrySpinner.setSelection(position);
+		}
+	}
+	
+	private void init(){
+		final Spinner countrySpinner = (Spinner) findViewById(R.id.countrySelector);
+		countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int pos, long id) {
+				SharedPreferences.Editor editor = prefs.edit();
+				editor.putString(COUNTRY_KEY, countrySpinner.getSelectedItem().toString());
+				editor.commit();
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				
+			}
+		});
 	}
 
 }

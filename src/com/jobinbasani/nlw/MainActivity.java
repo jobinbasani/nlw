6cd225d1e6df2ed5a7c3e1251432f0252f1b7172
jobinbasani <jobinbasani@gmail.com>
2013-12-02 15:16:28 -0400

    Add Analytics

--- src/com/jobinbasani/nlw/MainActivity.java
+++ src/com/jobinbasani/nlw/MainActivity.java
@@ -2,6 +2,7 @@ package com.jobinbasani.nlw;
 
 import java.util.List;
 
+import com.google.analytics.tracking.android.EasyTracker;
 import com.jobinbasani.nlw.sql.NlwDataContract.NlwDataEntry;
 import com.jobinbasani.nlw.sql.NlwDataDbHelper;
 import com.jobinbasani.nlw.util.NlwUtil;
@@ -44,6 +45,18 @@ public class MainActivity extends Activity {
 		prefs = getPreferences(MODE_PRIVATE);
 		
 		new DatabaseLoaderTask().execute();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		EasyTracker.getInstance(this).activityStart(this);
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		EasyTracker.getInstance(this).activityStop(this);
 	}
 
 	@Override

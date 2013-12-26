091d5525db02211e43c354ad26a5d180a0027d4a
jobinbasani <jobinbasani@gmail.com>
2013-12-26 15:06:32 -0400

    Replace context menu with overflow menu

--- src/com/jobinbasani/nlw/DetailsActivity.java
+++ src/com/jobinbasani/nlw/DetailsActivity.java
@@ -6,19 +6,15 @@ import com.jobinbasani.nlw.sql.NlwDataContract.NlwDataEntry;
 import com.jobinbasani.nlw.util.NlwUtil;
 
 import android.os.Bundle;
-import android.app.AlertDialog;
 import android.app.ListActivity;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView.AdapterContextMenuInfo;
-import android.widget.ListView;
+import android.view.ViewParent;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
 import android.widget.RelativeLayout;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -29,10 +25,6 @@ public class DetailsActivity extends ListActivity {
 	
 	private Cursor cursor;
 	private SQLiteDatabase db;
-	private static final int OPEN_CALENDAR_ID = 1;
-	private static final int ADD_EVENT_ID = 2;
-	private static final int READ_MORE_ID = 3;
-	private static final int SHARE_ID = 4;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,16 +32,15 @@ public class DetailsActivity extends ListActivity {
 		setContentView(R.layout.activity_details);
 		// Show the Up button in the action bar.
 		setupActionBar();
-		final ListView listView = getListView();
-		registerForContextMenu(listView);
+		
 		Intent detailsIntent = getIntent();
 		String country = detailsIntent.getStringExtra(MainActivity.COUNTRY_KEY);
 		TextView holidayCountryText = (TextView) findViewById(R.id.holidayCountryInfo);
 		holidayCountryText.setText(getResources().getString(R.string.upcomingWeekendsText)+" "+country);
 		cursor = getDetailsCursor(country);
 		
-		String[] from = new String[] { NlwDataEntry.COLUMN_NAME_NLWDATE, NlwDataEntry.COLUMN_NAME_NLWNAME, NlwDataEntry.COLUMN_NAME_NLWTEXT, NlwDataEntry.COLUMN_NAME_NLWDATE, NlwDataEntry.COLUMN_NAME_NLWDATE };
-	    int[] to = new int[] { R.id.detailDateText, R.id.detailHolidayName, R.id.detailHolidayDetails, R.id.detailYearText, R.id.detailMonthText};
+		String[] from = new String[] { NlwDataEntry.COLUMN_NAME_NLWDATE, NlwDataEntry.COLUMN_NAME_NLWNAME, NlwDataEntry.COLUMN_NAME_NLWTEXT, NlwDataEntry.COLUMN_NAME_NLWDATE, NlwDataEntry.COLUMN_NAME_NLWDATE, NlwDataEntry.COLUMN_NAME_NLWDATE };
+	    int[] to = new int[] { R.id.detailDateText, R.id.detailHolidayName, R.id.detailHolidayDetails, R.id.detailYearText, R.id.detailMonthText, R.id.overflowIcon};
 		SimpleCursorAdapter adapter = new SimpleCursorAdapter(getBaseContext(), R.layout.nlw_details, cursor, from, to, SimpleCursorAdapter.NO_SELECTION);
 		adapter.setViewBinder(new DetailsViewBinder());
 		setListAdapter(adapter);
@@ -68,51 +59,6 @@ public class DetailsActivity extends ListActivity {
 	}
 
 	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v,
-			ContextMenuInfo menuInfo) {
-		super.onCreateContextMenu(menu, v, menuInfo);
-		AdapterContextMenuInfo acmi = (AdapterContextMenuInfo) menuInfo;
-		RelativeLayout rl =  (RelativeLayout) acmi.targetView;
-		TextView dateText = (TextView) rl.findViewById(R.id.detailHolidayName);
-		menu.setHeaderTitle(dateText.getText()+"");
-		menu.setHeaderIcon(R.drawable.get_info);
-		menu.add(Menu.NONE, OPEN_CALENDAR_ID, 100, getResources().getString(R.string.detailsOpenCalendar));
-		menu.add(Menu.NONE, ADD_EVENT_ID, 200, getResources().getString(R.string.addEventAction));
-		menu.add(Menu.NONE, READ_MORE_ID, 300, getResources().getString(R.string.readMore));
-		menu.add(Menu.NONE, SHARE_ID, 400, getResources().getString(R.string.shareAction));
-	}
-
-	@Override
-	public boolean onContextItemSelected(MenuItem item) {
-		AdapterContextMenuInfo acmi = (AdapterContextMenuInfo) item.getMenuInfo();
-		RelativeLayout rl =  (RelativeLayout) acmi.targetView;
-		TextView dateText = (TextView) rl.findViewById(R.id.detailDateText);
-		TextView detailsText = (TextView) rl.findViewById(R.id.detailHolidayDetails);
-		TextView holidayText = (TextView) rl.findViewById(R.id.detailHolidayName);
-		
-		switch(item.getItemId()){
-		case OPEN_CALENDAR_ID:
-			startActivity(NlwUtil.getOpenCalendarIntent(Integer.parseInt(dateText.getTag()+"")));
-			break;
-		case ADD_EVENT_ID:
-			startActivity(NlwUtil.getAddEventIntent(Integer.parseInt(dateText.getTag()+""), holidayText.getText()+""));
-			break;
-		case READ_MORE_ID:
-			startActivity(NlwUtil.getReadMoreIntent(this,detailsText.getTag()+""));
-			break;
-		case SHARE_ID:
-			int dateNumber = Integer.parseInt(dateText.getTag()+"");
-			int year = (dateNumber/10000)*10000;
-			int month = (dateNumber - year)/100;
-			int date = dateNumber-(year+(month*100));
-			year = (year/10000)+2000;
-			startActivity(NlwUtil.getShareDataIntent(holidayText.getText()+" on "+NlwUtil.getMonthName(month, false)+" "+date+", "+year+" - "+detailsText.getText()+". "+getResources().getString(R.string.readMoreAt)+" "+detailsText.getTag()));
-			break;
-		}
-		return super.onContextItemSelected(item);
-	}
-
-	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		cursor.close();
@@ -128,12 +74,6 @@ public class DetailsActivity extends ListActivity {
 
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.details, menu);
-		return true;
-	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -147,20 +87,6 @@ public class DetailsActivity extends ListActivity {
 			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
 			//
 			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		case R.id.info_settings:
-			AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-			alertBuilder.setTitle(getResources().getString(R.string.infoDialogTitle))
-			.setMessage(getResources().getString(R.string.infoDialogMessage))
-			.setCancelable(false)
-			.setPositiveButton(getResources().getString(R.string.okButtonText), new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					
-				}
-			});
-			AlertDialog dialog = alertBuilder.create();
-			dialog.show();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -203,8 +129,59 @@ public class DetailsActivity extends ListActivity {
 				detailsText.setText(cursor.getString(columnIndex)+"");
 				detailsText.setTag(cursor.getString(cursor.getColumnIndexOrThrow(NlwDataEntry.COLUMN_NAME_NLWWIKI)));
 				return true;
+			}else if(view.getId() == R.id.overflowIcon){
+				final PopupMenu popupMenu = new PopupMenu(DetailsActivity.this, view);
+				popupMenu.getMenuInflater().inflate(R.menu.holiday_details, popupMenu.getMenu());
+				view.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						popupMenu.show();
+					}
+				});
+				popupMenu.setOnMenuItemClickListener(new DetailsMenuClick(view.getParent()));
+			
+				return true;
 			}
 			return false;
+		}
+		
+	}
+	
+	private class DetailsMenuClick implements OnMenuItemClickListener{
+		
+		private RelativeLayout rl;
+		
+		DetailsMenuClick(ViewParent view){
+			this.rl = (RelativeLayout) view;
+		}
+		
+		@Override
+		public boolean onMenuItemClick(MenuItem item) {
+			TextView dateText = (TextView) rl.findViewById(R.id.detailDateText);
+			TextView detailsText = (TextView) rl.findViewById(R.id.detailHolidayDetails);
+			TextView holidayText = (TextView) rl.findViewById(R.id.detailHolidayName);
+			
+			switch(item.getItemId()){
+			case R.id.detailsMenuOpenCalendar:
+				startActivity(NlwUtil.getOpenCalendarIntent(Integer.parseInt(dateText.getTag()+"")));
+				break;
+			case R.id.detailsMenuAddEvent:
+				startActivity(NlwUtil.getAddEventIntent(Integer.parseInt(dateText.getTag()+""), holidayText.getText()+""));
+				break;
+			case R.id.detailsMenuReadMore:
+				startActivity(NlwUtil.getReadMoreIntent(rl.getContext(),detailsText.getTag()+""));
+				break;
+			case R.id.detailsMenuShareAction:
+				int dateNumber = Integer.parseInt(dateText.getTag()+"");
+				int year = (dateNumber/10000)*10000;
+				int month = (dateNumber - year)/100;
+				int date = dateNumber-(year+(month*100));
+				year = (year/10000)+2000;
+				startActivity(NlwUtil.getShareDataIntent(holidayText.getText()+" on "+NlwUtil.getMonthName(month, false)+" "+date+", "+year+" - "+detailsText.getText()+". "+getResources().getString(R.string.readMoreAt)+" "+detailsText.getTag()));
+				break;
+			}
+			
+			return true;
 		}
 		
 	}
